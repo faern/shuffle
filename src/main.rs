bd79a2e067b46e5a174e1112fc2c9f7db29c4d9b
Linus Färnstrand <faern@faern.net>
2015-01-29 13:05:50 +0100

    Refactored code out of main()

--- src/main.rs
+++ src/main.rs
@@ -14,30 +14,42 @@ fn main() {
   }
 
   let mut arg_given = false;
-  let quote_style: QuoteStyle = if args.len() > 1 {
-    let arg0 = &args[1];
-    let mut arg0iter = arg0.chars();
-    if arg0iter.next() == Some('-') {
+  let quote_style: QuoteStyle = match parse_quote_style(&args[]) {
+    Some(q) => {
       arg_given = true;
-      match arg0iter.next() {
-        Some('n') => QuoteStyle::None,
-        Some('a') => QuoteStyle::All,
-        Some('s') => QuoteStyle::WithSpace,
-        _         => panic!("Invalid argument given"),
-      }
-    } else {
-      QuoteStyle::WithSpace
-    }
-  } else {
-    QuoteStyle::WithSpace
+      q
+    },
+    None => QuoteStyle::WithSpace, //Default quote style
   };
 
   let mut shuffle_list = args[if arg_given {2} else {1}..].to_vec();
-
   let mut rng = thread_rng();
   rng.shuffle(shuffle_list.as_mut_slice());
 
-  for item in shuffle_list.iter() {
+  print_args(&shuffle_list[], quote_style);
+}
+
+fn parse_quote_style(args: &[String]) -> Option<QuoteStyle> {
+  if args.len() > 1 {
+    let arg0 = &args[1];
+    let mut arg0iter = arg0.chars();
+    if arg0iter.next() == Some('-') {
+      match arg0iter.next() {
+        Some('n') => Some(QuoteStyle::None),
+        Some('a') => Some(QuoteStyle::All),
+        Some('s') => Some(QuoteStyle::WithSpace),
+        _         => panic!("Invalid argument given"),
+      }
+    } else {
+      None
+    }
+  } else {
+    None
+  }
+}
+
+fn print_args(args: &[String], quote_style: QuoteStyle) {
+  for item in args.iter() {
     let quote = match quote_style {
       QuoteStyle::None => false,
       QuoteStyle::All => true,
