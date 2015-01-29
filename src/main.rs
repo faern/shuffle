029d2396f4b9a93a04f78793554730420a8c4e9c
Linus Färnstrand <faern@faern.net>
2015-01-29 12:50:23 +0100

    Possible to give arg to control quotes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,52 @@
 use std::os;
 use std::rand::{thread_rng, Rng};
 
+enum QuoteStyle {
+  None,
+  All,
+  WithSpace,
+}
+
 fn main() {
-  let all_args = os::args();
-  let mut args = all_args[1..].to_vec();
+  let args = os::args();
+  if args.len() <= 1 {
+    return;
+  }
+
+  let mut arg_given = false;
+  let quote_style: QuoteStyle = if args.len() > 1 {
+    let arg0 = &args[1];
+    let mut arg0iter = arg0.chars();
+    if arg0iter.next() == Some('-') {
+      arg_given = true;
+      match arg0iter.next() {
+        Some('n') => QuoteStyle::None,
+        Some('a') => QuoteStyle::All,
+        Some('s') => QuoteStyle::WithSpace,
+        _         => panic!("Invalid argument given"),
+      }
+    } else {
+      QuoteStyle::WithSpace
+    }
+  } else {
+    QuoteStyle::WithSpace
+  };
+
+  let mut shuffle_list = args[if arg_given {2} else {1}..].to_vec();
 
   let mut rng = thread_rng();
-  rng.shuffle(args.as_mut_slice());
+  rng.shuffle(shuffle_list.as_mut_slice());
 
-  for arg in args.iter() {
-    print!("{} ", arg);
+  for item in shuffle_list.iter() {
+    let quote = match quote_style {
+      QuoteStyle::None => false,
+      QuoteStyle::All => true,
+      QuoteStyle::WithSpace => item.find(|&: c: char| c.is_whitespace()).is_some(),
+    };
+    match quote {
+      true => print!("\"{}\" ", item),
+      false => print!("{} ", item),
+    };
   }
   println!("");
 }
